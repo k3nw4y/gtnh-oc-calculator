b57808369f40cb5e96f6855fc5265811de9b57c4
k3nw4y <jarishannu@gmail.com>
2022-02-19 21:42:36 +0530

    cleanup

--- app.py
+++ app.py
@@ -49,26 +49,3 @@ def calculate():
         except Exception as error:
             result=" - Recipe Voltage Cannot Be Greater Than Machine Voltage -"
             return render_template('app.html', result=result)
-
-
-
-
-
-
-    # if operation == 'add':
-    #     result = float(num1) + float(num2)
-    #     return render_template('app.html', result=result)
-
-    # elif operation == 'subtract':
-    #     result = float(num1) - float(num2)
-    #     return render_template('app.html', result=result)
-
-    # elif operation == 'multiply':
-    #     result = float(num1) * float(num2)
-    #     return render_template('app.html', result=result)
-
-    # elif operation == 'divide':
-    #     result = float(num1) / float(num2)
-    #     return render_template('app.html', result=result)
-    # else:
-    #     return render_template('app.html')
